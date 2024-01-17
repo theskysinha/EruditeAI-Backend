d2bfe81f1865bb4162b3b7e4f29b856220065dad
Arya Sunil <91372517+Ariaa22@users.noreply.github.com>
2024-01-17 23:24:36 +0530

    Add files via upload

---
+++ htmltemplate.py
@@ -0,0 +1,48 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <div style="height: 90px; width: 90px; border-radius: 50%; background-color: #000; color: 333; display: flex; align-items: center; justify-content: center; font-weight: bold; font-size: 36px;">
+            &#129302; <!-- HTML entity for robot face -->
+        </div>
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <div style="height: 90px; width: 90px; border-radius: 50%; background-color: #f2f2f2; color: #fff; display: flex; align-items: center; justify-content: center; font-weight: bold; font-size: 36px;">
+            &#128100; <!-- HTML entity for human emoji -->
+        </div>
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
